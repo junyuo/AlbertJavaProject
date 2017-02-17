38930da9a90b8cf9b731ae40b9922aed45ba29aa
albert_kuo <albert_kuo@cht.com.tw>
2017-02-17 10:35:31 +0800

    added lambda example

--- albert-app/src/main/java/albert/practice/lambda/LambdaExample.java
+++ albert-app/src/main/java/albert/practice/lambda/LambdaExample.java
@@ -11,7 +11,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-public class LambdaTest {
+//https://dzone.com/articles/a-java-8-streams-cookbook?edition=268944&utm_source=Daily%20Digest&utm_medium=email&utm_campaign=dd%202017-02-14
+public class LambdaExample {
 
     public List<Person> filterData() {
         List<Person> people = createData();
@@ -25,12 +26,29 @@ public class LambdaTest {
                 .sorted((p1, p2) -> p2.name.compareTo(p1.name)).collect(Collectors.toList());
     }
 
+    public List<String> getPersonOfAgeGreaterThan40() {
+        List<Person> people = createData();
+        return people.stream().filter(p -> p.age >= 35).map(Person::getName)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getPersonOfAgeGreaterThan40WithLimit2() {
+        List<Person> people = createData();
+        return people.stream().filter(p -> p.age >= 35).map(Person::getName).limit(2)
+                .collect(Collectors.toList());
+    }
+
+    public long countPeople() {
+        List<Person> people = createData();
+        return people.stream().map(Person::getName).count();
+    }
+
     public List<Person> createData() {
         Person ben = new Person("Ben Whittaker", 70, "ben@gmail.com");
         Person jules = new Person("Jules Ostin", 30, "jules@gmail.com");
         Person fiona = new Person("Fiona Farwell", 68, null);
         Person matt = new Person("Matt", 35, "matt@gmail.com");
-        
+
         return Arrays.asList(ben, jules, fiona, matt);
     }
 
--- albert-app/src/test/java/albert/practice/lambda/LambdaUnitTest.java
+++ albert-app/src/test/java/albert/practice/lambda/LambdaUnitTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
 
-import albert.practice.lambda.LambdaTest.Person;
+import albert.practice.lambda.LambdaExample.Person;
 import lombok.extern.slf4j.Slf4j;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
@@ -16,11 +16,11 @@ import nl.jqno.equalsverifier.Warning;
 @Slf4j
 public class LambdaUnitTest {
 
-    private LambdaTest lambdaTest = null;
+    private LambdaExample lambdaTest = null;
 
     @Before
     public void setup() {
-        lambdaTest = new LambdaTest();
+        lambdaTest = new LambdaExample();
     }
 
     @Test
@@ -43,16 +43,36 @@ public class LambdaUnitTest {
         assertEquals(people.size(), 3);
     }
 
+    @Test
+    public void testGetPersonOfAgeGreaterThan40() {
+        List<String> names = lambdaTest.getPersonOfAgeGreaterThan40();
+        log.debug("names = " + names.toString());
+        assertEquals(names.size(), 3);
+    }
+
+    @Test
+    public void testGetPersonOfAgeGreaterThan40WithLimit2() {
+        List<String> names = lambdaTest.getPersonOfAgeGreaterThan40WithLimit2();
+        log.debug("names with limit 2 = " + names.toString());
+        assertEquals(names.size(), 2);
+    }
+
+    @Test
+    public void testCountPeople() {
+        long count = lambdaTest.countPeople();
+        assertEquals(count, 4);
+    }
+
     // Testing getter and setters methods
     @Test
     public void testPersonBean() {
-        new BeanTester().testBean(LambdaTest.Person.class);
+        new BeanTester().testBean(LambdaExample.Person.class);
     }
 
     // Testing equals() and hashCode()
     @Test
     public void testEqualsAndHashcode() {
-        EqualsVerifier.forClass(LambdaTest.Person.class)
+        EqualsVerifier.forClass(LambdaExample.Person.class)
                 .suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
     }
 
