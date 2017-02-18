d01c939cf84e2a3fa9f232cf1cd3a58088e80434
albert_kuo <albert_kuo@cht.com.tw>
2017-02-18 11:13:05 +0800

    add send email by exchange server example

---
+++ albert-app/src/main/java/albert/practice/mail/SendMailByExchangeServerExample.java
@@ -0,0 +1,97 @@
+package albert.practice.mail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamSource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import lombok.Data;
+
+public class SendMailByExchangeServerExample {
+
+    public void sendMail(EmailParams params) {
+        JavaMailSenderImpl sender = getJavaMailSender();
+        MimeMessage message = sender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(params.getReceiver());
+            helper.setFrom(params.getSender());
+            helper.setSubject(params.getSubject());
+            helper.setText(params.getContent(), true);
+
+            List<File> attachments = params.getAttachments();
+            if (CollectionUtils.isNotEmpty(attachments)) {
+                for (File file : attachments) {
+                    helper.addAttachment(file.getName(), file);
+                }
+            }
+
+            if (params.getImgMap() != null) {
+                for (Map.Entry<String, String> entry : params.getImgMap().entrySet()) {
+                    InputStreamSource logo = new ByteArrayResource(
+                            IOUtils.toByteArray(getClass().getResourceAsStream(entry.getValue())));
+                    helper.addInline(entry.getKey(), logo, "image/png");
+                }
+            }
+
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+        sender.setDefaultEncoding("UTF-8");
+        sender.send(message);
+    }
+
+    private JavaMailSenderImpl getJavaMailSender() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.ssl.trust", "*");
+
+        // mail server configuration
+        String host = "test.test.com.tw";
+        int port = 25;
+        String userName = "test";
+        String password = "test";
+
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setJavaMailProperties(props);
+        sender.setHost(host);
+        sender.setPort(port);
+        sender.setUsername(userName);
+        sender.setPassword(password);
+        sender.setDefaultEncoding("UTF-8");
+
+        return sender;
+    }
+
+    @Data
+    public static class EmailParams {
+        private String sender;
+        private String receiver;
+        private String subject;
+        private String content;
+        private List<File> attachments;
+        private Map<String, String> imgMap;
+    }
+
+    @Data
+    public static class Customer {
+        private String policyNumber;
+        private String name;
+        private String applyDate;
+        private String fromDate;
+        private String toDate;
+        private String place;
+    }
+
+}
---
+++ albert-app/src/test/java/albert/practice/mail/TestSendMailByExchangeServerExample.java
@@ -0,0 +1,68 @@
+package albert.practice.mail;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.ui.velocity.VelocityEngineUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestSendMailByExchangeServerExample {
+
+    private SendMailByExchangeServerExample.EmailParams params = null;
+    private SendMailByExchangeServerExample.Customer customer = null;
+    private SendMailByExchangeServerExample sendMailService;
+
+    @Before
+    public void setup() {
+        params = new SendMailByExchangeServerExample.EmailParams();
+        params.setSender("test@cht.com.tw");
+        params.setReceiver("test@cht.com.tw");
+        params.setSubject("電子郵件測試 (Email test)");
+        params.setAttachments(Arrays.asList(new File("D:/dropbox/test_測試.pdf"),
+                new File("D:/dropbox/Getting Started.pdf")));
+        Map<String, String> imgMap = new HashMap<>();
+        imgMap.put("Chansey", "img/Chansey.png");
+        imgMap.put("Hitmonchan", "img/Hitmonchan.png");
+        imgMap.put("Pikachu", "img/Pikachu.png");
+        params.setImgMap(imgMap);
+        
+        customer = new SendMailByExchangeServerExample.Customer();
+        customer.setPolicyNumber("12345678");
+        customer.setName("測試");
+        customer.setApplyDate("20170201");
+        customer.setFromDate("20170228");
+        customer.setToDate("20160410");
+        customer.setPlace("日本關西");
+
+        sendMailService = new SendMailByExchangeServerExample();
+    }
+
+    @Test
+    public void testSendMail() {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        VelocityEngine velocityEngine = (VelocityEngine) context.getBean("velocityEngine");
+        
+        // set customer to map for velocity email template
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("customer", customer);
+        
+        // get email content from velocity email template
+        String mailTemplate = "albert/practice/mail/template/template.vm";
+        String content = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, mailTemplate, "UTF-8", model);
+        log.debug("content="+content);
+        params.setContent(content);
+        
+        sendMailService.sendMail(params);
+    }
+
+}
