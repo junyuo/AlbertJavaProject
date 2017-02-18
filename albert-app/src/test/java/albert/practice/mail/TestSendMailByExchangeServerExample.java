a5ff7dd334c4f997f73a96c69682b973931f7f8b
albert_kuo <albert_kuo@cht.com.tw>
2017-02-18 16:00:17 +0800

    improve TestSendMailByExchangeServerExample code coverage

--- albert-app/src/test/java/albert/practice/mail/TestSendMailByExchangeServerExample.java
+++ albert-app/src/test/java/albert/practice/mail/TestSendMailByExchangeServerExample.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import org.apache.velocity.app.VelocityEngine;
 import org.junit.Before;
 import org.junit.Test;
+import org.meanbean.test.BeanTester;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import lombok.extern.slf4j.Slf4j;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 
 @Slf4j
 public class TestSendMailByExchangeServerExample {
@@ -34,7 +37,7 @@ public class TestSendMailByExchangeServerExample {
         imgMap.put("Hitmonchan", "img/Hitmonchan.png");
         imgMap.put("Pikachu", "img/Pikachu.png");
         params.setImgMap(imgMap);
-        
+
         customer = new SendMailByExchangeServerExample.Customer();
         customer.setPolicyNumber("12345678");
         customer.setName("ด๚ธี");
@@ -51,18 +54,40 @@ public class TestSendMailByExchangeServerExample {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
         VelocityEngine velocityEngine = (VelocityEngine) context.getBean("velocityEngine");
-        
+
         // set customer to map for velocity email template
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("customer", customer);
-        
+
         // get email content from velocity email template
         String mailTemplate = "albert/practice/mail/template/template.vm";
-        String content = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, mailTemplate, "UTF-8", model);
-        log.debug("content="+content);
+        String content = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, mailTemplate,
+                "UTF-8", model);
+        log.debug("content=" + content);
         params.setContent(content);
-        
+
         sendMailService.sendMail(params);
     }
 
+    @Test
+    public void testEmailParamsBean() {
+        new BeanTester().testBean(SendMailByExchangeServerExample.EmailParams.class);
+    }
+
+    @Test
+    public void testCustomerBean() {
+        new BeanTester().testBean(SendMailByExchangeServerExample.Customer.class);
+    }
+
+    @Test
+    public void testEmailParamsBeanEqualsAndHashcode() {
+        EqualsVerifier.forClass(SendMailByExchangeServerExample.EmailParams.class)
+                .suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
+    }
+    
+    @Test
+    public void testCustomerBeanEqualsAndHashcode() {
+        EqualsVerifier.forClass(SendMailByExchangeServerExample.Customer.class)
+                .suppress(Warning.STRICT_INHERITANCE, Warning.NONFINAL_FIELDS).verify();
+    }
 }
