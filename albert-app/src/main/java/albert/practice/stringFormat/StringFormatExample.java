036583cdb76df8819282e94ea26b37f10d6a92cc
albert_kuo <albert_kuo@cht.com.tw>
2017-04-07 15:20:15 +0800

    add new message format example

--- albert-app/src/main/java/albert/practice/stringFormat/StringFormatExample.java
+++ albert-app/src/main/java/albert/practice/stringFormat/StringFormatExample.java
@@ -1,9 +1,17 @@
 package albert.practice.stringFormat;
 
+import java.text.Format;
+import java.text.MessageFormat;
+
 public class StringFormatExample {
 
     public String returnWith1000Separator(int amount) {
         return String.format("%,d", amount);
+    }
+    
+    public String getFormattedMessage(String message, String... values) {
+       Format messageFormat = new MessageFormat(message);
+       return messageFormat.format(values);
     }
     
 }
