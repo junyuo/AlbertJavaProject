4ac6f3b61c6ef7b9c92f39bb6493369fe97e7ce1
albert_kuo <albert_kuo@cht.com.tw>
2017-08-10 18:01:29 +0800

    add PersonResourceTest

---
+++ albert-app/src/test/java/albert/practice/guice/PersonResourceTest.java
@@ -0,0 +1,30 @@
+package albert.practice.guice;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.inject.Guice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PersonResourceTest {
+	
+	private PersonResource personResouce;
+	
+	@Before
+	public void setup() {
+		personResouce = Guice.createInjector().getInstance(PersonResource.class);
+	}
+	
+	@Test
+	public void testGetContacts() {
+		List<Contact> contacts = personResouce.getContacts("testId");
+		contacts.stream().forEach(c -> log.debug(c.toString()));
+		assertEquals(3, contacts.size());
+	}
+}
