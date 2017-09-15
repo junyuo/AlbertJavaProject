f18cf18ab684c459c6bab5966c29a37d21bf6628
albert_kuo <albert_kuo@cht.com.tw>
2017-09-15 14:31:33 +0800

    add new examples in NumericUtils and its test class

--- albert-app/src/main/java/albert/practice/validation/NumericUtils.java
+++ albert-app/src/main/java/albert/practice/validation/NumericUtils.java
@@ -1,60 +1,94 @@
 package albert.practice.validation;
 
+import org.apache.commons.validator.routines.DoubleValidator;
 import org.apache.commons.validator.routines.IntegerValidator;
 
-import lombok.extern.slf4j.Slf4j;
-
-// https://commons.apache.org/proper/commons-validator/apidocs/org/apache/commons/validator/routines/package-summary.html#package_description
-@Slf4j
 public class NumericUtils {
 
-	public static void main(String[] args) {
-		NumericUtils.isValidNumeric("123");
-		log.debug("test=" + NumericUtils.formatNumeric(1235600));
-		log.debug("Integer = " + NumericUtils.formatNumberString("1,235,600"));
-		NumericUtils.isInValidRange(20, 18, 65);
-	}
-
 	/**
-	 * 檢查輸入的 number string 是否為數字.
+	 * 檢查輸入的 integer string 是否為數字.
 	 * 
 	 * @param number
 	 */
-	public static void isValidNumeric(String number) {
+	public static void isValidInteger(String number) {
 		if (IntegerValidator.getInstance().validate(number) == null) {
 			throw new IllegalArgumentException("您輸入的非數字");
 		}
 	}
 
 	/**
-	 * 將 number 加上 1000 separator.
+	 * 檢查輸入的 double string 是否為數字.
+	 * 
+	 * @param number
+	 */
+	public static void isValidDouble(String number) {
+		if (DoubleValidator.getInstance().validate(number) == null) {
+			throw new IllegalArgumentException("您輸入的非數字");
+		}
+	}
+
+	/**
+	 * 將 integer 加上 1000 separator.
 	 * 
 	 * @param number
 	 * @return
 	 */
-	public static String formatNumeric(Integer number) {
+	public static String formatInteger(Integer number) {
 		return IntegerValidator.getInstance().format(number, "#,##0");
 	}
 
 	/**
-	 * 將number string 轉成 integer.
+	 * 將 Double 加上 1000 separator.
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static String formatDouble(Double number) {
+		return DoubleValidator.getInstance().format(number, "#,##0.00");
+	}
+
+	/**
+	 * 將integer string 轉成 Integer.
 	 * 
 	 * @param numberStr
 	 * @return
 	 */
-	public static Integer formatNumberString(String numberStr) {
+	public static Integer formatIntegerString(String numberStr) {
 		return IntegerValidator.getInstance().validate(numberStr, "#,##0");
 	}
 
 	/**
-	 * 檢查數字是否介於指定區間.
+	 * 將double string 轉成 Double.
+	 * 
+	 * @param numberStr
+	 * @return
+	 */
+	public static Double formatDoubleString(String numberStr) {
+		return DoubleValidator.getInstance().validate(numberStr, "#,##0.00");
+	}
+
+	/**
+	 * 檢查 integer 是否介於指定區間.
 	 * 
 	 * @param number
 	 * @param min
 	 * @param max
 	 */
-	public static void isInValidRange(Integer number, int min, int max) {
+	public static void isIntegerInValidRange(Integer number, int min, int max) {
 		if (!IntegerValidator.getInstance().isInRange(number, min, max)) {
+			throw new IllegalArgumentException("您輸入的數字必須介於 " + min + " ~ " + max + " 之間");
+		}
+	}
+
+	/**
+	 * 檢查 double 是否介於指定區間.
+	 * 
+	 * @param number
+	 * @param min
+	 * @param max
+	 */
+	public static void isDoubleInValidRange(Double number, double min, double max) {
+		if (!DoubleValidator.getInstance().isInRange(number, min, max)) {
 			throw new IllegalArgumentException("您輸入的數字必須介於 " + min + " ~ " + max + " 之間");
 		}
 	}
--- albert-app/src/test/java/albert/practice/validation/NumericUtilsTest.java
+++ albert-app/src/test/java/albert/practice/validation/NumericUtilsTest.java
@@ -7,35 +7,83 @@ import org.junit.Test;
 
 public class NumericUtilsTest {
 
-	private String number;
-	private String invalidNumber;
+	private String integerString;
+	private String invalidIntegerString;
+
+	private String doubleString;
+	private String invaliddoubleString;
 
 	@Before
 	public void setup() {
-		number = "12345000";
-		invalidNumber = "1000a";
+		integerString = "12345000";
+		invalidIntegerString = "1000a";
+
+		doubleString = "12400.60";
+		invaliddoubleString = "12400a.60";
 	}
 
 	@Test
-	public void testValidNumber() {
-		NumericUtils.isValidNumeric(number);
+	public void testValidInteger() {
+		NumericUtils.isValidInteger(integerString);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidNumber() {
-		NumericUtils.isValidNumeric(invalidNumber);
+	public void testInvalidInteger() {
+		NumericUtils.isValidInteger(invalidIntegerString);
 	}
 
 	@Test
-	public void testFormatNumberToString() {
-		String numberString = NumericUtils.formatNumeric(12345600);
+	public void testFormatIntegerToString() {
+		String numberString = NumericUtils.formatInteger(12345600);
 		assertEquals("12,345,600", numberString);
 	}
 
 	@Test
-	public void testFormatNumStringToNumber() {
-		Integer number = NumericUtils.formatNumberString("12,345,600");
+	public void testFormatNumStringToInteger() {
+		Integer number = NumericUtils.formatIntegerString("12,345,600");
 		assertEquals(new Integer(12345600), number);
+	}
+
+	@Test
+	public void testIntegerIsInRange() {
+		NumericUtils.isIntegerInValidRange(new Integer(22), 20, 30);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testIntegerIsNotInRange() {
+		NumericUtils.isIntegerInValidRange(new Integer(35), 20, 30);
+	}
+
+	@Test
+	public void testValidDouble() {
+		NumericUtils.isValidDouble(doubleString);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidDouble() {
+		NumericUtils.isValidDouble(invaliddoubleString);
+	}
+
+	@Test
+	public void testFormatDouble(){
+		String number = NumericUtils.formatDouble(new Double(12300));
+		assertEquals("12,300.00", number);
+	}
+	
+	@Test
+	public void testFormatStringToDouble() {
+		Double number = NumericUtils.formatDoubleString(doubleString);
+		assertEquals(new Double(12400.60), number);
+	}
+
+	@Test
+	public void testDoubleIsInRange() {
+		NumericUtils.isDoubleInValidRange(new Double(20.5), 20.0, 30.0);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testDoubleIsNotInRange() {
+		NumericUtils.isDoubleInValidRange(new Double(30.5), 20.0, 30.0);
 	}
 
 }
